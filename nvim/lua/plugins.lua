ba2da949111c1175d1ed6505a2a694e6f8fec4cc
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-09-28 21:28:12 +0530

    general run

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,90 +1,95 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system(
+        {
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath
+        }
+    )
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    cmd = "Telescope",
-    opts = function()
-      return require "nvchad.configs.telescope"
-    end,
-    config = function(_, opts)
-      local telescope = require "telescope"
-      telescope.setup(opts)
+        {
+            "nvim-lualine/lualine.nvim",
+            dependencies = {"nvim-tree/nvim-web-devicons"}
+        },
+        {
+            "nvim-telescope/telescope.nvim",
+            dependencies = {"nvim-treesitter/nvim-treesitter"},
+            cmd = "Telescope",
+            opts = function()
+                return require "nvchad.configs.telescope"
+            end,
+            config = function(_, opts)
+                local telescope = require "telescope"
+                telescope.setup(opts)
 
-      -- load extensions
-      for _, ext in ipairs(opts.extensions_list) do
-        telescope.load_extension(ext)
-      end
-    end,
-  },
-  {
-    'xeluxee/competitest.nvim',
-    dependencies = 'MunifTanjim/nui.nvim',
-    config = function() require('competitest').setup() end,
-  },
- 
-  {
-    "onsails/lspkind.nvim",
-    event = { "VimEnter" },
-  },
-  -- LSP manager
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
-  -- Auto-completion engine
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "lspkind.nvim",
-      "hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
-      "hrsh7th/cmp-buffer", -- buffer auto-completion
-      "hrsh7th/cmp-path", -- path auto-completion
-      "hrsh7th/cmp-cmdline", -- cmdline auto-completion
-    },
-    config = function()
-      require("config.nvim-cmp")
-    end,
-  },
-  -- Auto-Pairs
-  {
-    "windwp/nvim-autopairs",
-    opts = {
-      fast_wrap = {},
-      disable_filetype = { "TelescopePrompt", "vim" },
-    },
-    config = function(_, opts)
-      require("nvim-autopairs").setup(opts)
+                -- load extensions
+                for _, ext in ipairs(opts.extensions_list) do
+                    telescope.load_extension(ext)
+                end
+            end
+        },
+        {
+            "xeluxee/competitest.nvim",
+            dependencies = "MunifTanjim/nui.nvim",
+            config = function()
+                require("competitest").setup()
+            end
+        },
+        {
+            "onsails/lspkind.nvim",
+            event = {"VimEnter"}
+        },
+        -- LSP manager
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        -- Auto-completion engine
+        {
+            "hrsh7th/nvim-cmp",
+            dependencies = {
+                "lspkind.nvim",
+                "hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+                "hrsh7th/cmp-buffer", -- buffer auto-completion
+                "hrsh7th/cmp-path", -- path auto-completion
+                "hrsh7th/cmp-cmdline" -- cmdline auto-completion
+            },
+            config = function()
+                require("config.nvim-cmp")
+            end
+        },
+        -- Auto-Pairs
+        {
+            "windwp/nvim-autopairs",
+            opts = {
+                fast_wrap = {},
+                disable_filetype = {"TelescopePrompt", "vim"}
+            },
+            config = function(_, opts)
+                require("nvim-autopairs").setup(opts)
 
-      -- setup cmp for autopairs
-      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-      require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
-  },
-  -- Code snippet engine
-  {
-    "L3MON4D3/LuaSnip",
-    version = "v2.*",
-  },
-  "tamton-aquib/keys.nvim", 
-  -- Theme
-  "loctvl842/monokai-pro.nvim",
-  "catppuccin/nvim",
-  "navarasu/onedark.nvim",
-})
+                -- setup cmp for autopairs
+                local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+                require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+            end
+        },
+        -- Code snippet engine
+        {
+            "L3MON4D3/LuaSnip",
+            version = "v2.*"
+        },
+        -- Key screen
+        "tamton-aquib/keys.nvim",
+        -- Theme
+        "loctvl842/monokai-pro.nvim",
+        "catppuccin/nvim",
+        "navarasu/onedark.nvim"
+    }
+)
 
