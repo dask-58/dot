26cef9ec76bc8a502b0630ed459423b0e9ee3f42
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-09-28 21:25:15 +0530

    general run

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,8 +5,18 @@ require('colorscheme')
 require('lsp')
 require('competitest').setup()
 require('lualine').setup() -- {
+ --    sections = {
+   --     lualine_a = {
+     --  {
+       --         'datetime',
+                -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
+         --       style = 'default'
+           -- }
+        --}
+    --}
     --  options = { theme = material }
- -- }
+--}
+--
 require("keys").setup {
     enable_on_startup = true,
     win_opts = {
