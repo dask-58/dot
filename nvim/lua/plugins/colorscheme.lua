ac9147e6192d268cd726558e98662f53d88e82b0
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-10-27 12:21:17 +0530

    ui changes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
   { "rebelot/kanagawa.nvim" },
-
+  { "projekt0n/github-nvim-theme", name = "github-theme" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   -- Load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "catppuccin",
     },
   },
 }
