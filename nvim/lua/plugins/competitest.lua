d7c498503d033f4cbf7e9714543772817cdd7e8e
Dhruv Koli <140686560+dask-58@users.noreply.github.com>
2024-10-27 22:14:27 +0530

    small fix

--- nvim/lua/plugins/competitest.lua
+++ nvim/lua/plugins/competitest.lua
@@ -1,11 +1,9 @@
 return {
   {
-    {
-      "xeluxee/competitest.nvim",
-      dependencies = "MunifTanjim/nui.nvim",
-      config = function()
-        require("competitest").setup()
-      end,
-    },
+    "xeluxee/competitest.nvim",
+    dependencies = "MunifTanjim/nui.nvim",
+    config = function()
+      require("competitest").setup()
+    end,
   },
 }
