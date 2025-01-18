78a61d710ef8303fad61bdfaff64a4cfb11e80ef
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-01-18 11:59:11 +0530

    Add: theme Eldritch

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,11 +2,12 @@ return {
   { "rebelot/kanagawa.nvim" },
   { "projekt0n/github-nvim-theme", name = "github-theme" },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "eldritch-theme/eldritch.nvim", lazy = false, priority = 1000, opts = {} },
   -- Load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "eldritch",
     },
   },
 }
