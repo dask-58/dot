8f129f6d922d7f17c683fd38c253d73def8b316d
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-03-30 17:52:29 +0530

    Add: autocommand to open dashboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Open Dashboard by default if no file passed by cmd.
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.cmd("Dashboard")
+    end
+  end,
+})
