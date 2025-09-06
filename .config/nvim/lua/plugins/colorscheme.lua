3582667b0e6df4762833ea4b81d103eebedee38c
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-09-06 20:28:13 +0530

    long time

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- kanagawa : [wave, dragon, lotus]
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
+  },
+}
